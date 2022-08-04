ed39964770d2c9337182c9d3c9d1eac1eb5a9718
Beulah Mathenge <beulahmathenge@gmail.com>
2022-08-04 16:30:11 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+ 
+const names= ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    var cardMessages=[]
+  for (let i = 0; i < names.length; i++) {
+    var messages = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    console.log (" Yellow" + messages);
+    cardMessages.push(messages);
+    debugger;
+  }
+
+  return cardMessages;
+}
+writeCards(names);
+
+//While loop
+function countDown(){
+let i=10;
+while (i> -1){
+    console.log(i--);
+}
+}
+countDown();
